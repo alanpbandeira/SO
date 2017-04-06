56b2682aa788faf0024643501d4e2dc942e7260d
Alan Bandeira <alan.p.bandeira@gmail.com>
2017-04-06 16:07:38 -0300

    p/c finish

--- producer_consumer.py
+++ producer_consumer.py
@@ -1,28 +1,51 @@
 import time
+import threading
+import math
 
+count = 0
 buffer_size = 100
 buffer = []
+chand = threading.Condition()
 
-def producer():
-    global buffer
+def producer(start_time):
+    global buffer, chand, count
 
     while True:
-        if len(buffer) >= buffer_size:
-            time.sleep(1)
+        if len(buffer) == buffer_size:
+            chand.acquire()
+            chand.wait()
         
+        if time.time() - start_time >= 1:
+            if math.floor(time.time() - start_time) % 10 == 0:
+                start_time = time.time()
+                print(1)
         buffer.append(1)
         
         if buffer:
-            pass # wakeup consumer
+            chand.acquire()
+            chand.notify()
 
 def consumer():
-    global buffer
+    global buffer, chand
 
     while True:
         if not buffer:
-            time.sleep(1)
+            chand.acquire()
+            chand.wait()
         
         buffer.pop()
 
         if not buffer:
-            pass # wakeup produce
+            chand.acquire()
+            chand.notify()
+
+start = time.time()
+
+try:
+    threading._start_new_thread(producer, (start, ))
+    threading._start_new_thread(consumer, ())
+except:
+    print("Error, unable to start threads")
+
+while 1:
+    pass
