48a26a002bd0b3821f21cf014fab18f8d8972f56
Alan Bandeira <alan.p.bandeira@gmail.com>
2017-06-06 22:21:36 -0300

    small fixes

--- so_file_serach_project/app.py
+++ so_file_serach_project/app.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import ttk, scrolledtext, Frame
+from tkinter import ttk, scrolledtext, Frame, Text, Scrollbar
 
 
 class App(Frame):
@@ -9,30 +9,33 @@ class App(Frame):
         super(App, self).__init__(master)
         self.master = master
         self.s_string = tk.StringVar()
-        self.results = []
 
-        # self.master = tk.Tk()
         self.master.title('File Search')
+        # self.pack(fill='both', expand='yes')
+        self.master.resizable(0, 0)
 
         # Adding the label
         string_label = ttk.Label(self.master, text='Search String: ')
         string_label.grid(column=0, row=0)
+        # string_label.pack(side="top")
 
         # Adding a textbox
-        self.input_data = ttk.Entry(self.master, width=12, textvariable=self.s_string)
+        self.input_data = ttk.Entry(self.master, width=20, textvariable=self.s_string)
         self.input_data.focus()
         self.input_data.grid(column=1, row=0)
+        # self.input_data.pack(side="top")
 
         # Adding a Button
         self.action = ttk.Button(self.master, text="Search", command=self.string_search)
         self.action.grid(column=2, row=0)
+        # self.action.pack(side="top")
 
         # Adding scrolled textbox
-        # scroll_w = 30
-        # scroll_h = 3
-        # self.scr_textbox = scrolledtext.ScrolledText(
-        #     self.master, width=scroll_w, height=scroll_h, wrap=tk.WORD)
-        # self.scr_textbox.grid(column=0, columnspan=3)
+        scroll_w = 100
+        scroll_h = 15
+        self.scr_textbox = scrolledtext.ScrolledText(
+            self.master, width=scroll_w, height=scroll_h, wrap=tk.WORD)
+        self.scr_textbox.grid(column=0, columnspan=10)
 
         self.run()
 
@@ -41,6 +44,7 @@ class App(Frame):
 
     def string_search(self):
         stg = self.s_string.get().lower()
+        results = []
 
         if stg == '':
             print(stg)
@@ -53,17 +57,15 @@ class App(Frame):
                 # print(os.path.join(dirname, subdirname))
                 path = os.path.join(dirname, subdirname)
                 if stg in path.lower():
-                    self.results.append(path)
-                    print(self.results)
+                    results.append(path)
 
             for filename in filenames:
                 # print(os.path.join(dirname, filename))
                 path = os.path.join(dirname, filename)
                 if stg in path.lower():
-                    self.results.append(path)
+                    results.append(path)
 
         print('finished searching')
-        paths = "\n".join(self.results)
+        paths = "\n".join(results) + "\n"
 
-        result_label = ttk.Label(self.master, text=paths)
-        result_label.grid(column=0, columnspan=3)
+        self.scr_textbox.insert('end', paths)
