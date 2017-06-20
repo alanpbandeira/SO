dba2da4762f96ee9a6ca0668b0add38aedfef5b1
Alan Bandeira <alan.p.bandeira@gmail.com>
2017-06-20 18:08:40 -0300

    minor changes

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
-def foo(bar, baz):
-    print ('hello {0}'.format(bar))
-    return 'foo' + baz
+from pdfminer.pdfparser import PDFParser
+# from pdfminer.pdfdocument import PDFDocument
+# from pdfminer.pdfpage import PDFPage
+# from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+# from pdfminer.pdfinterp import PDFResourceManager
+# from pdfminer.pdfinterp import PDFPageInterpreter
+# from pdfminer.pdfdevice import PDFDevice
 
-from multiprocessing.pool import ThreadPool
-pool = ThreadPool(processes=1)
-
-# tuple of args for foo
-async_result = pool.apply_async(foo, ('world', 'foo'))
-
-# do some other stuff in the main process
-
-# get the return value from your function.
-return_val = async_result.get()
-
-print(return_val)
-
-print(type(return_val))
+# # Open a PDF file.
+# fp = open('so_file_serach_project/SO questões arquivo.pdf', 'rb')
+# # Create a PDF parser object associated with the file object.
+# parser = PDFParser(fp)
+# # Create a PDF document object that stores the document structure.
+# # Supply the password for initialization.
+# document = PDFDocument(parser, password)
+# # Check if the document allows text extraction. If not, abort.
+# if not document.is_extractable:
+#     raise PDFTextExtractionNotAllowed
+# # Create a PDF resource manager object that stores shared resources.
+# rsrcmgr = PDFResourceManager()
+# # Create a PDF device object.
+# device = PDFDevice(rsrcmgr)
+# # Create a PDF interpreter object.
+# interpreter = PDFPageInterpreter(rsrcmgr, device)
+# # Process each page contained in the document.
+# for page in PDFPage.create_pages(document):
+#     interpreter.process_page(page)
